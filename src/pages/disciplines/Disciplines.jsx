c64d702d681fb6f7c87e6fe45a1827cd6c8f2977
Nelli Rossi <nnross@utu.fi>
2025-01-09 17:05:40 +0200

    added disciplines

--- src/pages/disciplines/Disciplines.jsx
+++ src/pages/disciplines/Disciplines.jsx
@@ -1,7 +1,70 @@
 import propTypes from "prop-types";
 
 const Disciplines = ({ className = "disciplines", id = "disciplines" }) => (
-  <div className={className} id={id}></div>
+  <div className={className} id={id}>
+    <h1 className={`${className}__user__title`}>user research</h1>
+    <img
+      className={`${className}__user__img`}
+      src="../src/assets/user_research.svg"
+    />
+    <p className={`${className}__user__text`}>
+      I have knowledge of how to understand the people who use a product through
+      means like interviews, surveys, and usability testing. I can use my
+      understanding to create meaningful, user-centred products.
+    </p>
+    <h1 className={`${className}__visual__title`}>visual design</h1>
+    <img
+      className={`${className}__visual__img`}
+      src="../src/assets/visual_design.svg"
+    />
+    <p className={`${className}__visual__text`}>
+      I have a keen eye for designing the visual qualities of a product in an
+      aesthetically pleasing way. My passion for great visual design can be seen
+      on my focus on typography, colour theory, and layout principles.
+    </p>
+    <h1 className={`${className}__info__title`}>information architecture</h1>
+    <img
+      className={`${className}__info__img`}
+      src="../src/assets/information_a.svg"
+    />
+    <p className={`${className}__info__text`}>
+      I’m able to identify and organise information within a product in a way
+      that enables users to find information effortlessly. Specific techniques
+      I’m familiar with include card sorting and tree testing.
+    </p>
+    <h1 className={`${className}__interaction__title`}>interaction design</h1>
+    <img
+      className={`${className}__interaction__img`}
+      src="../src/assets/interaction_design.svg"
+    />
+    <p className={`${className}__interaction__text`}>
+      I know how to design the interactive behaviours of a product so that they
+      ensure that the user’s journey is logical, efficient, and enjoyable.
+    </p>
+    <h1 className={`${className}__usability__title`}>usability evaluation</h1>
+    <img
+      className={`${className}__usability__img`}
+      src="../src/assets/usability_evaluation.svg"
+    />
+    <p className={`${className}__usability__text`}>
+      I have experience on evaluating the usability of a product based on
+      Nielsen’s heuristics. I can also make changes on designs base on insights
+      to make sure the final product meets user needs efficiently.
+    </p>
+    <h1 className={`${className}__accessibility__title`}>
+      accessibility evaluation
+    </h1>
+    <img
+      className={`${className}__accessibility__img`}
+      src="../src/assets/accessibility_evaluation.svg"
+    />
+    <p className={`${className}__accessibility__text`}>
+      I have a firm understanding of accessibility standards, like the WCAG, and
+      I’m able to measure the quality of a product to be accessed by everyone.
+      For example checking for colour contrast, keyboard navigation, and screen
+      reader compatibility.
+    </p>
+  </div>
 );
 
 export default Disciplines;
